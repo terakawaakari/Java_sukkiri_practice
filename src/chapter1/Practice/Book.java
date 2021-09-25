4ec7266acb13d04492c72fa781cddba5e6961b85
terakawaakari <terakawaakari0701@gmail.com>
2021-09-25 19:43:00 +0900

    [Fix]practice1-1

--- src/chapter1/Practice/Book.java
+++ src/chapter1/Practice/Book.java
@@ -1,5 +1,38 @@
 package chapter1.Practice;
 
-public class Book {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+public class Book implements Cloneable {
+	private String title;
+	private Date publishDate;
+	private String comment;
+	
+	public boolean equals(Book book) {
+		if (book.title.equals(title) && book.publishDate == publishDate) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	List<Book> bookList = new ArrayList<>();
+	
+	public int compareTo(Book book) {
+		if (this.publishDate.before(book.publishDate)) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+	
+	public Book clone() {
+		Book result = new Book();
+		result.title = this.title;
+		result.publishDate = this.publishDate;
+		result.comment = this.comment;
+		return result;
+	}
+	
 }
