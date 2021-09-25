baef47facc212d2d3734bc07cf9a9c1d09550311
terakawaakari <terakawaakari0701@gmail.com>
2021-09-25 19:54:34 +0900

    [Finish]practice1-2

--- src/chapter1/Practice/Book.java
+++ src/chapter1/Practice/Book.java
@@ -1,38 +1,43 @@
 package chapter1.Practice;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.Objects;
 
 public class Book implements Cloneable {
 	private String title;
 	private Date publishDate;
 	private String comment;
-	
-	public boolean equals(Book book) {
-		if (book.title.equals(title) && book.publishDate == publishDate) {
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public int hashCode() {
+		return Objects.hash(this.title, this.publishDate, this.comment);
+	}
+
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null) return false;
+		if (!(o instanceof Book)) return false;
+		Book b = (Book) o;
+		if (this.title.trim().equals(b.title.trim()) && this.publishDate.equals(b.publishDate)) {
 			return true;
-		} else {
-			return false;
 		}
+		return false;
+
 	}
-	
-	List<Book> bookList = new ArrayList<>();
-	
+
 	public int compareTo(Book book) {
-		if (this.publishDate.before(book.publishDate)) {
-			return -1;
-		} else {
-			return 1;
-		}
+		return this.publishDate.compareTo(book.publishDate);
 	}
-	
+
 	public Book clone() {
 		Book result = new Book();
 		result.title = this.title;
-		result.publishDate = this.publishDate;
+		result.publishDate = (Date) this.publishDate.clone();
 		result.comment = this.comment;
 		return result;
 	}
-	
+
 }
