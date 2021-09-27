e5fd462cbdcc9623b913d00f8cc4b1f0fc218a9d
terakawaakari <terakawaakari0701@gmail.com>
2021-09-27 13:29:54 +0900

    [Finish] practice3-2

--- src/chapter3/practice/Main.java
+++ src/chapter3/practice/Main.java
@@ -2,9 +2,8 @@ package chapter3.practice;
 
 public class Main {
 	public static void main(String[] args) {
-		FuncList list = new FuncList();
-		Func1 f1 = FuncList::isOdd;
-		Func2 f2 = list::passCheck;
+		Func1 f1 = (x) -> {return (x % 2 == 1);};
+		Func2 f2 = (point, name) -> {return name + "さんは" + (point > 65 ? "合格" : "不合格");};
 		
 		System.out.println(f1.isOdd(15));
 		System.out.println(f2.passCheck(80, "佐藤"));
