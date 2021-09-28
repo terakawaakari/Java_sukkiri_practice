dd5fed93c3bbe58cdc2711b4eafc806a56346541
terakawaakari <terakawaakari0701@gmail.com>
2021-09-28 16:32:38 +0900

    [Fix] practice4-2

--- src/chapter4/practice/MemoryEater.java
+++ src/chapter4/practice/MemoryEater.java
@@ -1,5 +1,19 @@
 package chapter4.practice;
 
-public class MemoryEater {
+import java.util.Locale;
 
+public class MemoryEater {
+	public static void main(String[] args) {
+		Locale loc = Locale.getDefault();
+		String lang = loc.getLanguage();
+		if (lang.equals("ja")) {
+			System.out.println("メモリを消費しています...");
+		} else {
+			System.out.println("eating memory...");
+		}
+		long[] memory = new long[1280000];
+		for (int i = 0; i < memory.length; i++) {
+			memory[i] = i;
+		}
+	}
 }
