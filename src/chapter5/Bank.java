fb7ebc65da8073f5c7aa3c89e7891ffe7c552245
terakawaakari <terakawaakari0701@gmail.com>
2021-09-28 23:25:25 +0900

    [Finish] chapter5

---
+++ src/chapter5/Bank.java
@@ -0,0 +1,12 @@
+package chapter5;
+
+import org.apache.commons.lang3.builder.*;
+
+public class Bank {
+	String name;
+	String address;
+
+	public boolean equals(Object o) {
+		return EqualsBuilder.reflectionEquals(this, o);
+	}
+}
